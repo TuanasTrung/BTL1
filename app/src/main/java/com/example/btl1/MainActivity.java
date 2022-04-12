302633953bf3dc4e0c9a577a026fbdb30d13ec4a
Chinh <Chinhvu1031@gmail.com>
2022-04-13 01:37:40 +0700

    thêm login này

--- app/src/main/java/com/example/btl1/MainActivity.java
+++ app/src/main/java/com/example/btl1/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.btl1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView username =(TextView) findViewById (R.id.inputEmail);
+        TextView password =(TextView) findViewById (R.id.inputPassword);
+
+        Button Loginbtn = (Button) findViewById(R.id.btnLogin);
+
+        Loginbtn.setOnClickListener (new View.OnClickListener () {
+            @Override
+            public void onClick(View view) {
+                if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+                    Toast.makeText( MainActivity.this, "LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText( MainActivity.this, "LOGIN FAILED",Toast.LENGTH_SHORT).show();}
+            }
+        });
     }
 }
