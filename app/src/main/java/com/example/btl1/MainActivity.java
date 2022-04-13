d8c25c4fc46b0c83f84dd7744a819923c3e74807
TuanasTrung <91027912+TuanasTrung@users.noreply.github.com>
2022-04-13 16:41:08 +0700

    trung

--- app/src/main/java/com/example/btl1/MainActivity.java
+++ app/src/main/java/com/example/btl1/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.btl1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,24 +14,36 @@ import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button b1,b2;
+    EditText ed1,ed2;
+    TextView tx1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView username =(TextView) findViewById (R.id.inputEmail);
-        TextView password =(TextView) findViewById (R.id.inputPassword);
+        b1 = (Button) findViewById(R.id.btndn);
+        b2 = (Button) findViewById(R.id.btndk);
+        ed1 = (EditText) findViewById(R.id.edtk);
+        ed2 = (EditText) findViewById(R.id.edmk);
 
-        Button Loginbtn = (Button) findViewById(R.id.btnLogin);
-
-        Loginbtn.setOnClickListener (new View.OnClickListener () {
+        b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
-                    Toast.makeText( MainActivity.this, "LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
+                if(ed1.getText().toString().equals("admin") &&
+                ed2.getText().toString().equals("admin")){
+                    Intent intent = new Intent(MainActivity.this, home.class);
+                    startActivity(intent);
                 }
-                else {
-                    Toast.makeText( MainActivity.this, "LOGIN FAILED",Toast.LENGTH_SHORT).show();}
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(MainActivity.this, register.class);
+                startActivity(intent1);
             }
         });
     }
--- app/src/main/java/com/example/btl1/register.java
+++ app/src/main/java/com/example/btl1/register.java
@@ -2,13 +2,28 @@ package com.example.btl1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class register extends AppCompatActivity {
+
+    Button b3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        b3 = (Button) findViewById(R.id.btndk1);
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2 = new Intent(register.this, MainActivity.class);
+                startActivity(intent2);
+            }
+        });
     }
 }
